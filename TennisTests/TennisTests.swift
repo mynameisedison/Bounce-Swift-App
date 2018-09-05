e9cf586d3e6bfa212e03417c8adf31d5c04d8f33
Edison <edisontoole@gmail.com>
2018-09-05 14:10:44 -0600

    Initial Commit

---
+++ TennisTests/TennisTests.swift
@@ -0,0 +1,36 @@
+//
+//  TennisTests.swift
+//  TennisTests
+//
+//  Created by Edison Toole on 9/5/18.
+//  Copyright © 2018 Edison Toole. All rights reserved.
+//
+
+import XCTest
+@testable import Tennis
+
+class TennisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
